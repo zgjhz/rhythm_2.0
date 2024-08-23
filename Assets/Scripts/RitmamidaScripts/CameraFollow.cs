624799232c362a9b2c80b8c0c7b1b1302e94dca5
Ignat <ignatio_04@mail.ru>
2024-08-23 15:23:16 +0300

    Second version

--- Assets/Scripts/RitmamidaScripts/CameraFollow.cs
+++ Assets/Scripts/RitmamidaScripts/CameraFollow.cs
@@ -5,7 +5,6 @@ public class CameraFollow : MonoBehaviour
     public GameObject target; // Цель, за которой будет следовать камера (например, последняя линия)
     public float smoothSpeed = 0.125f; // Скорость плавного следования камеры
     public Vector3 offset; // Смещение камеры относительно цели
-    public GameObject mainCamera;
 
     private void LateUpdate()
     {
@@ -13,7 +12,7 @@ public class CameraFollow : MonoBehaviour
         {
             Vector3 desiredPosition = target.transform.position + offset; // Желаемая позиция камеры
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); // Плавное движение камеры
-            mainCamera.transform.position = smoothedPosition; // Установка новой позиции камеры
+            transform.position = smoothedPosition; // Установка новой позиции камеры
         }
         
     }
