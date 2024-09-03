59e4af315ccabcf7580c7fb71639de982e4d4a23
zgjhz <2558386@gmail.com>
2024-09-03 22:58:29 +0300

    kinda working version

--- Assets/Scripts/GayometryDash/PlayerMoveScript.cs
+++ Assets/Scripts/GayometryDash/PlayerMoveScript.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerMoveScript : MonoBehaviour
 {
     public Rigidbody2D player;
+    public float angularVelocity;
+    private float gravity = 20f;
+    public float velocity = 5f;
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -12,9 +16,37 @@ public class PlayerMoveScript : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "spike") {
+            Debug.Log("you lose");
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        player.angularVelocity = 5f;
+        if (Input.GetKeyDown("space") && isGrounded)
+        {
+            angularVelocity = 90 * gravity / (velocity * 2);
+            player.angularVelocity = -angularVelocity;
+            player.velocity = new Vector2(0f, velocity);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("floor"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("floor"))
+        {
+            isGrounded = false;
+        }
     }
 }
