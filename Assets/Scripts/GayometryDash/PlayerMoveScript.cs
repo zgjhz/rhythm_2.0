91674ea5d6086580e621e65451587e1ddca6bd82
Gleb Golub <2558386@gmail.com>
2024-09-14 15:34:53 +0300

    ya eblan i delal v main

--- Assets/Scripts/GayometryDash/PlayerMoveScript.cs
+++ Assets/Scripts/GayometryDash/PlayerMoveScript.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class PlayerMoveScript : MonoBehaviour
 {
     public Rigidbody2D player;
-    public float angularVelocity;
-    private float gravity = 20f;
-    public float velocity = 5f;
+    private float angularVelocity;
+    private float fixedGravity = 20f;
+    private float velocity = 5f;
     private bool isGrounded;
+    public MenuManager menuManager;
+    public float rayDistance = 2f;
+
+    private float interval;
+    private float gravity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        interval = menuManager.interval;
+        player.gravityScale = -0.34f * interval + 1.13f;
+        gravity = (-0.34f * interval + 1.13f) * fixedGravity;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,27 +33,45 @@ public class PlayerMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        interval = menuManager.interval;
+        player.gravityScale = -0.34f * interval + 1.13f;
+        gravity = (-0.34f * interval + 1.13f) * fixedGravity;
+        Debug.Log(gravity);
+        velocity = gravity * interval / 2;
         if (Input.GetKeyDown("space") && isGrounded)
         {
             angularVelocity = 90 * gravity / (velocity * 2);
             player.angularVelocity = -angularVelocity;
             player.velocity = new Vector2(0f, velocity);
         }
-    }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("floor"))
+        RaycastHit2D hit = Physics2D.Raycast(player.position, Vector2.down, rayDistance, LayerMask.GetMask("Ground"));
+        RaycastHit2D hit_left = Physics2D.Raycast(player.position, new Vector2(0.2f, -1), rayDistance, LayerMask.GetMask("Ground"));
+        RaycastHit2D hit_right = Physics2D.Raycast(player.position, new Vector2(-0.2f, -1), rayDistance, LayerMask.GetMask("Ground"));
+
+        if (hit.collider != null || hit_left.collider != null || hit_right.collider != null)
         {
             isGrounded = true;
         }
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("floor"))
+        else
         {
             isGrounded = false;
         }
     }
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("floor"))
+    //    {
+    //        isGrounded = true;
+    //    }
+    //}
+
+    //void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("floor"))
+    //    {
+    //        isGrounded = false;
+    //    }
+    //}
 }
