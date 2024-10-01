dc9ff289234b6cc1c415bf7d26001d510496038b
Gleb Golub <2558386@gmail.com>
2024-10-01 22:22:06 +0300

    working version with sliding barrel

--- Assets/Scripts/ArrowGame/ArrowController.cs
+++ Assets/Scripts/ArrowGame/ArrowController.cs
@@ -6,23 +6,25 @@ public class ArrowController : MonoBehaviour
     public MenuManager menuManager;
     public GameObject newArrow;
     public Transform spawnPoint;
-    public float rotationSpeed = 30f; 
+    public float movementSpeed = 5f;
+    public float movementRange = 12f;
     public float arrowSpeed = 10f; 
     private bool isFired = false; 
     private Vector3 direction;
     private float interval = 1f;
     private int isFirst = 0;
-    private float angle = 0f;
     private float testTimer = 0.5f;
     private bool isInMenu = false;
+    private bool shouldMove = true;
+    private float oldTime = 0;
 
     private void Start()
     {
         interval = menuManager.interval;
-        rotationSpeed = 60 / interval;
-        transform.rotation = Quaternion.Euler(0, 0, 0);
-        angle = 0;
-        testTimer = 0.5f;
+        Debug.Log("On start" + interval);
+        movementSpeed = movementRange / interval;
+        testTimer = interval;
+        direction = Vector3.right;
     }
 
     void Update()
@@ -31,44 +33,68 @@ public class ArrowController : MonoBehaviour
         {
             isFirst = 1;
         }
-        if (isFirst == 1 || isFirst == 2 && !isInMenu) {
+        if (isFirst == 1 || isFirst == 2 && !isInMenu)
+        {
             testTimer -= Time.deltaTime;
-            if (!isFired)
+            MoveBow();
+            if (!isFired && !isInMenu)
             {
-                angle = Mathf.PingPong(Time.time * rotationSpeed, 60) - 30; // Вращение от -30 до +30
-                transform.rotation = Quaternion.Euler(0, 0, angle); // Вращение вокруг оси Y
                 newArrow.transform.position = spawnPoint.position;
                 newArrow.transform.rotation = transform.rotation;
-                if (testTimer <= 0 && isFirst == 2)
+                if (Input.GetKeyDown(KeyCode.Space) && isFirst == 2)
                 {
                     testTimer = interval;
-                    FireArrow();
+                    FireArrow(Time.time - oldTime);
+                    Debug.Log(Time.time - oldTime + "     " + interval);
+                    oldTime = Time.time;
                 }
                 isFirst = 2;
             }
         }
     }
 
+    private void MoveBow()
+    {
+        transform.Translate(direction * movementSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction = -direction;
+            //Debug.Log(transform.position.x);
+        }
+    }
+
+    //void Resume() {
+    //    shouldMove = true;
+    //}
+
     public void OnMenuOpened() {
         isInMenu = true;
+        //shouldMove = false;
     }
 
     public void OnIntervalChanged() {
         interval = menuManager.interval;
-        rotationSpeed = 60 / interval;
-        transform.rotation = Quaternion.Euler(0, 0, 0);
-        angle = 0;
+        movementSpeed = movementRange / interval;
+        transform.position = new Vector3(0, -3.5f, 0);
         isInMenu = false;
-        Debug.Log("Intreval changed HUUUY" + transform.rotation);
+        testTimer = interval;
+        //Invoke("Resume", 2f);
+        Debug.Log("Intreval changed HUUUY" + transform.position);
     }
 
-    void FireArrow()
+    void FireArrow(float deltaTime)
     {
         isFired = false;
-        direction = transform.up;
         Rigidbody rb = newArrow.GetComponent<Rigidbody>();
-        rb.velocity = direction * arrowSpeed;
+        rb.velocity = transform.up * arrowSpeed;
         rb.freezeRotation = true;
+        if (Mathf.Abs(deltaTime - interval) / interval <= 0.1) {
+            newArrow.transform.position = new Vector3(0, 0, 0);
+        }
         SpawnNewArrow();
         // Создание новой стрелы через время
         //Invoke("SpawnNewArrow", 2f);
